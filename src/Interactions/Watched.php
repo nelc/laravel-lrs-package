5f6962a42f5dffa3f835ec2c7504920936f5d0e6
OmarHesham_66 <62832339+OmarHesham66@users.noreply.github.com>
2025-11-18 18:17:47 +0200

    fix watched

--- src/Interactions/Watched.php
+++ src/Interactions/Watched.php
@@ -34,73 +34,72 @@ class Watched
 
     }
 
-    public function Send( $actor, $actorEmail, $lessonUrl, $lessonTitle, $lessonDesc, bool $completion, $duration, $courseId, $courseTitle, $courseDesc, $instructor, $instructorEmail ){
+    public function Send($actor, $actorEmail, $lessonUrl, $lessonTitle, $lessonDesc, bool $completion, $duration, $courseId, $courseTitle, $courseDesc, $instructor, $instructorEmail)
+    {
         $this->init();
         $data = array(
             'actor' => array(
-                        'name' => strval($actor),
-                        'mbox'  => 'mailto:'.strval($actorEmail),
-                        'objectType' => 'Agent',
-                    ),
+                'name' => strval($actor),
+                'mbox' => 'mailto:' . strval($actorEmail),
+                'objectType' => 'Agent',
+            ),
             'verb' => array(
-                        'id' => 'https://w3id.org/xapi/acrossx/verbs/watched',
-                        'display' => array("en-US" => "watched") 
-                    ),
+                'id' => 'https://w3id.org/xapi/acrossx/verbs/watched',
+                'display' => array("en-US" => "watched")
+            ),
             'object' => array(
-                            'id'=> strval($lessonUrl),
-                            'definition' => array(
-                                'name' => array("en-US"=> strval($lessonTitle)),
-                                'description' => array("en-US"=> strval($lessonDesc)),
-                                'type' => 'https://w3id.org/xapi/video/activity-type/video'
-                            ),
-                            'objectType' => 'Activity',
-                        ),
+                'id' => strval($lessonUrl),
+                'definition' => array(
+                    'name' => array("en-US" => strval($lessonTitle)),
+                    'description' => array("en-US" => strval($lessonDesc)),
+                    'type' => 'https://w3id.org/xapi/video/activity-type/video'
+                ),
+                'objectType' => 'Activity',
+            ),
             'context' => array(
-                            'instructor' => array(
-                                'name' => strval($instructor),
-                                'mbox' => 'mailto:'.strval($instructorEmail),
+                'instructor' => array(
+                    'name' => strval($instructor),
+                    'mbox' => 'mailto:' . strval($instructorEmail),
+                ),
+                'platform' => strval($this->platform),
+                'language' => strval($this->lang),
+                'extensions' => array(
+                    "http://id.tincanapi.com/extension/browser-info" => array(
+                        "code_name" => strval($this->browserCode),
+                        "name" => strval($this->browserName),
+                        "version" => strval($this->browserVersion)
+                    ),
+                    "https://nelc.gov.sa/extensions/platform" => array(
+                        "name" => array(
+                            "ar-SA" => strval($this->platform_in_arabic),
+                            "en-US" => strval($this->platform_in_english)
+                        )
+                    )
+                ),
+                'contextActivities' => array(
+                    'parent' => array(
+                        array(
+                            'id' => strval($courseId),
+                            'definition' => array(
+                                'name' => array("en-US" => strval($courseTitle)),
+                                'description' => array("en-US" => strval($courseDesc)),
+                                'type' => 'https://w3id.org/xapi/cmi5/activitytype/course'
                             ),
-                            'platform' => strval($this->platform),
-                            'language' => strval($this->lang),
-                            'extensions' => array (
-                                "http://id.tincanapi.com/extension/browser-info" => array(
-                                    "code_name" => strval($this->browserCode),
-                                    "name" => strval($this->browserName),  
-                                    "version" => strval($this->browserVersion)
-                                ),
-                                array(
-                                    "https://nelc.gov.sa/extensions/platform" => array(
-                                        "name" => array(
-                                            "ar-SA" => strval($this->platform_in_arabic),
-                                            "en-US" => strval($this->platform_in_english)
-                                        )
-                                    )
-                                )
-                            ),
-                            'contextActivities' => array(
-                                'parent' => array(
-                                    array (
-                                        'id' => strval($courseId),
-                                        'definition' => array(  
-                                            'name' => array("en-US" => strval($courseTitle)),
-                                            'description' => array( "en-US" => strval($courseDesc) ),
-                                            'type' => 'https://w3id.org/xapi/cmi5/activitytype/course'
-                                        ),
-                                        'objectType' => "Activity"
-                                    ),
-                                    
-                                    
-                                )
-                            )
+                            'objectType' => "Activity"
                         ),
-                        'result' => array(
-                            'completion' => $completion,
-                            'duration' => $duration,
-                        ),
-            'timestamp' => date('Y-m-d\TH:i:s'.substr((string)microtime(), 1, 4).'\Z')
+
+
+                    )
+                )
+            ),
+            'result' => array(
+                'completion' => $completion,
+                'duration' => $duration,
+            ),
+            'timestamp' => date('Y-m-d\TH:i:s' . substr((string) microtime(), 1, 4) . '\Z')
         );
 
         return $data;
     }
-    
+
 }
